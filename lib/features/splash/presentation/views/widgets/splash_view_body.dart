645eeeb24284112ba46ccb634bc0564d03facd2f
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-07-26 14:09:30 +0300

    - Refactor SplashViewBody to StatefulWidget and implement navigation to OnboardingView

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,22 +1,43 @@
+import 'package:ecommerce_fruits/core/routing/routes.dart';
 import 'package:ecommerce_fruits/core/utils/app_assets.dart';
+import 'package:ecommerce_fruits/core/utils/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToOnboardingView();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Align(
           alignment: AlignmentDirectional.topStart,
           child: SvgPicture.asset(Assets.imagesPlant),
         ),
         SvgPicture.asset(Assets.imagesLogo),
-        SvgPicture.asset(Assets.imagesSplashBottom),
+        SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill,),
       ],
     );
+  }
+
+  void navigateToOnboardingView() {
+    Future.delayed(const Duration(seconds: 3), (){
+      context.pushReplacementNamed(Routes.onBoardingView);
+    });
   }
 }
