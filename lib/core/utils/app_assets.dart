c146393e3d23c3f1d6fc3af8426ce08cf58796bd
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-07-27 15:07:02 +0300

    - Add new asset constants for fruit and background images in app_assets.dart

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -2,9 +2,25 @@
 class Assets {
   Assets._();
   
+  /// Assets for imagesFruitBasket
+  /// assets/images/fruit_basket.svg
+  static const String imagesFruitBasket = "assets/images/fruit_basket.svg";
+
+  /// Assets for imagesGreenBackground
+  /// assets/images/green_background.svg
+  static const String imagesGreenBackground = "assets/images/green_background.svg";
+
   /// Assets for imagesLogo
   /// assets/images/logo.svg
   static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesOrangeBackground
+  /// assets/images/orange_background.svg
+  static const String imagesOrangeBackground = "assets/images/orange_background.svg";
+
+  /// Assets for imagesPineapple
+  /// assets/images/pineapple.svg
+  static const String imagesPineapple = "assets/images/pineapple.svg";
 
   /// Assets for imagesPlant
   /// assets/images/plant.svg
