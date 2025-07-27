2c14dfa8c5a6c8192c71f288126cfd8b3d3915b8
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-07-27 15:12:55 +0300

    - Added AppColors utility class for centralized color management

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF1B5E37);
+  static const Color secondary = Color(0xFFF4A91F);
+  static const Color grey950 = Color(0xFF0C0D0D);
+  static const Color gray500 = Color(0xFF4E5556);
+  static const Color gray400 = Color(0xFF949D9E);
+
+}
