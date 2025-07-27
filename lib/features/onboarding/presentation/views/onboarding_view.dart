5231a4175e176dae52af73477e2b373ee592cb2c
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-07-27 15:13:25 +0300

    - Added OnboardingView widget for onboarding screen

---
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -0,0 +1,13 @@
+import 'package:ecommerce_fruits/features/onboarding/presentation/views/widgets/onboarding_view_body.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const OnboardingViewBody(),
+    );
+  }
+}
