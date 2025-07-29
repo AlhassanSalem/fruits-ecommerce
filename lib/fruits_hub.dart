a68b458bbc0cc6b1fcdd49c0d214473c3a256693
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-07-29 13:33:31 +0300

    - enhance FruitsHub and main initialization with theming and Firebase setup

--- lib/fruits_hub.dart
+++ lib/fruits_hub.dart
@@ -1,6 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:ecommerce_fruits/core/routing/app_router.dart';
 import 'package:ecommerce_fruits/core/routing/routes.dart';
+import 'package:ecommerce_fruits/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class FruitsHub extends StatelessWidget {
@@ -19,7 +20,30 @@ class FruitsHub extends StatelessWidget {
       locale: Locale('ar'),
       theme: ThemeData(
         fontFamily: 'Cairo',
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          onPrimary: Colors.white,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: outlineInputBorder(),
+          focusedBorder: outlineInputBorder(),
+          enabledBorder: outlineInputBorder(),
+          filled: true,
+          fillColor: Color(0xFFF9FAFA),
+        ),
+        
       ),
     );
+  }
+
+  OutlineInputBorder outlineInputBorder({Color? borderColor}) {
+    return OutlineInputBorder(
+          borderRadius: BorderRadius.circular(4),
+          borderSide: BorderSide(color: borderColor ?? Color(0xffE6E9EA), width: 1),
+        );
   }
 }
