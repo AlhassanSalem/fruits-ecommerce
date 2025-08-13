052e2fdfada29cfcdd9133ed54b31421fec6fe27
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-08-13 11:14:23 +0300

    feat: update routing to include BlocProvider for sign in and sign up views

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,28 +1,48 @@
+import 'package:ecommerce_fruits/core/di/dependency_injection.dart';
 import 'package:ecommerce_fruits/core/routing/routes.dart';
-import 'package:ecommerce_fruits/features/auth/presentaion/views/login_view.dart';
+import 'package:ecommerce_fruits/features/auth/presentaion/cubits/signin/signin_cubit.dart';
+import 'package:ecommerce_fruits/features/auth/presentaion/cubits/signup/signup_cubit.dart';
+import 'package:ecommerce_fruits/features/auth/presentaion/views/signin_view.dart';
 import 'package:ecommerce_fruits/features/auth/presentaion/views/sign_up_view.dart';
 import 'package:ecommerce_fruits/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:ecommerce_fruits/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   static Route? generateRoute(RouteSettings settings) {
-    switch(settings.name) {
-      case Routes.splashView : 
+    switch (settings.name) {
+      case Routes.splashView:
         return MaterialPageRoute(builder: (context) => const SplashView());
 
-        case Routes.onBoardingView : 
+      case Routes.onBoardingView:
         return MaterialPageRoute(builder: (context) => const OnboardingView());
 
-        case Routes.loginView : 
-        return MaterialPageRoute(builder: (context) => const LoginView());
+      case Routes.signInView:
+        return MaterialPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) => getIt<SigninCubit>(),
+            child: const SignInView(),
+          ),
+        );
 
-        case Routes.signUpView : 
-        return MaterialPageRoute(builder: (context) => const SignUpView());
+      case Routes.signUpView:
+        return MaterialPageRoute(
+          builder: (context) => BlocProvider(
+            create: (context) => getIt<SignupCubit>(),
+            child: const SignUpView(),
+          ),
+        );
 
 
-      default : 
-         return null;
+      case Routes.homeView:
+        return MaterialPageRoute(
+          builder: (context) => const Scaffold(
+            body: Center(child: Text('Home View')),
+          ),
+        );
+      default:
+        return null;
     }
-  } 
+  }
 }
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,7 @@
 abstract class Routes {
   static const String splashView = '/splashView';
   static const String onBoardingView = '/onBoardingView';
-  static const String loginView = '/loginView';
+  static const String signInView = '/signInView';
   static const String signUpView = '/signUpView';
+  static const String homeView = '/homeView';
 }
