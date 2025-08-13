41ee7490db572c81f990ddb36553a55785608eed
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-08-13 11:24:56 +0300

    feat: create SignInView and remove obsolete LoginViewBody

--- lib/features/auth/presentaion/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentaion/views/widgets/sign_up_view_body.dart
@@ -1,46 +1,37 @@
 import 'package:ecommerce_fruits/core/helper/constants.dart';
-import 'package:ecommerce_fruits/core/helper/locale_keys.dart';
 import 'package:ecommerce_fruits/core/helper/spacing.dart';
-import 'package:ecommerce_fruits/core/widgets/custom_button.dart';
-import 'package:ecommerce_fruits/core/widgets/custom_text_form_field.dart';
 import 'package:ecommerce_fruits/features/auth/presentaion/views/widgets/have_account_widget.dart';
+import 'package:ecommerce_fruits/features/auth/presentaion/views/widgets/signup_button_bloc_consumer.dart';
+import 'package:ecommerce_fruits/features/auth/presentaion/views/widgets/signup_form.dart';
 import 'package:ecommerce_fruits/features/auth/presentaion/views/widgets/terms_and_conditions_widget.dart';
 import 'package:flutter/material.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
+   
 
   @override
   Widget build(BuildContext context) {
+    bool isAcceptedTermsAndConditions = false;
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsetsGeometry.symmetric(horizontal: kHorizontalPadding),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            verticalSpacing(kVerticalPadding),
-            CustomTextFormField(
-              hintText: LocaleKeys.fullName,
-            ),
-            verticalSpacing(kVerticalPadding),
-            CustomTextFormField(
-              hintText: LocaleKeys.email,
-              textInputType: TextInputType.emailAddress,
-            ),
-            verticalSpacing(kVerticalPadding),
-            CustomTextFormField(
-              hintText: LocaleKeys.password,
-              textInputType: TextInputType.visiblePassword,
-            ),
-            verticalSpacing(kVerticalPadding),
+            verticalSpacing(16),
+            const SignupForm(),
+            verticalSpacing(16),
             const TermsAndConditionsWidget(),
             verticalSpacing(30),
-            CustomButton(title: LocaleKeys.createNewAccount, onPressed: () {}),
+            SignupButtonBlocConsumer(
+              isAcceptedTermsAndConditions: isAcceptedTermsAndConditions,
+            ),
             verticalSpacing(26),
             const HaveAccountWidget(),
           ],
         ),
       ),
     );
-  }
+  } 
 }
