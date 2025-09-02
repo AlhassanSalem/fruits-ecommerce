98c0766f3eac29f97e5766a1544fdcb9e718c1b8
alhassanSalem <alhassanalwahaidi@hotmail.com>
2025-09-02 14:13:41 +0300

    feat: update localization files with new keys and translations for enhanced user experience

--- lib/core/helper/locale_keys.dart
+++ lib/core/helper/locale_keys.dart
@@ -1,6 +1,6 @@
 abstract class LocaleKeys {
-  // Onboarding 
-  static const welcomeTo = 'welcome_to'; 
+  // Onboarding
+  static const welcomeTo = 'welcome_to';
   static const onboardingSubtitle1 = 'onboarding_subtitle1';
   static const searchAndShop = 'search_and_shop';
   static const onboardingSubtitle2 = 'onboarding_subtitle2';
@@ -23,7 +23,8 @@ abstract class LocaleKeys {
   static const newAccount = 'new_account';
   static const fullName = 'full_name';
   static const createAccount = 'create_account';
-  static const byCreatingAnAccountYouAgreeTo = 'by_creating_an_account_you_agree_to';
+  static const byCreatingAnAccountYouAgreeTo =
+      'by_creating_an_account_you_agree_to';
   static const termsAndConditions = 'terms_and_conditions';
   static const createNewAccount = 'create_new_account';
   static const alreadyHaveAnAccount = 'already_have_an_account';
@@ -43,4 +44,33 @@ abstract class LocaleKeys {
   static const wrongPassword = 'wrong_password';
   static const invalidCredential = 'invalid_credential';
 
+  // Home
+  static const searchFor = 'search_for';
+  static const bestSelling = 'best_selling';
+  static const seeMore = 'see_more';
+
+  // Bottom Navigation Labels
+  static const home = 'home';
+  static const products = 'products';
+  static const shoppingCart = 'shopping_cart';
+  static const profile = 'profile';
+
+  /// Products View
+  static const results = 'results';
+  static const reviews = 'reviews';
+  static const addToCart = 'add_to_cart';
+  static const organic = 'organic';
+  static const expiration = 'expiration';
+  static const calories = 'calories';
+  static const gram = 'gram';
+  static const enterYourReview = 'enter_your_review';
+  static const cart = 'cart';
+  static const youHave = 'you_have';
+  static const productsInYourCart = 'products_in_your_cart';
+
+  static const productAddedToCart = 'product_added_to_cart';
+  static const productRemovedFromCart = 'product_removed_from_cart';
+
+  static const checkout = 'checkout';
+  static const egp = 'egp';
 }
